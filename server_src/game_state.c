a0a36bf62058c5edbd1454ea86949c4c6929cdc5
Tomasz Kasperek <kasperekt@gmail.com>
2016-02-17 19:11:48 +0100

    (server) game state stored locally

--- server_src/game_state.c
+++ server_src/game_state.c
@@ -1,2 +1,43 @@
+#include <stdlib.h>
 #include <stdio.h>
 #include "game_state.h"
+
+game_state_t *players[2] = { NULL, NULL };
+
+void init_players() {
+  players[0] = NULL;
+  players[1] = NULL;
+}
+
+void add_player(int player_id) {
+  if (player_id < 1 || player_id > 2) {
+    fprintf(stderr, "Trying to add new player failed (wrong id)\n");
+    return;
+  }
+
+  int id = player_id - 1;
+  if (players[id] != NULL) {
+      fprintf(stderr, "Player already taken\n");
+      return;
+  }
+
+  army_t *army = malloc(sizeof(struct army));
+  army->light = 0;
+  army->heavy = 0;
+  army->cavalry = 0;
+  army->workers = 0;
+
+  players[id] = malloc(sizeof(struct game_state));
+  players[id]->player_id = player_id;
+  players[id]->resources = INITIAL_RESOURCES_COUNT;
+  players[id]->army = army;
+}
+
+void destroy_players() {
+  for (int i = 0; i < 2; ++i) {
+    if (players[i] != NULL) {
+      free(players[i]->army);
+      free(players[i]);
+    }
+  }
+}
--- server_src/game_state.h
+++ server_src/game_state.h
@@ -2,14 +2,18 @@
 #define GAME__STATE__H
 #include "../shared_src/game_protocol.h"
 
+#define INITIAL_RESOURCES_COUNT 300
+
 typedef struct game_state {
+  short player_id;
   unsigned int resources;
-  army_t army;
+  army_t *army;
 } game_state_t;
 
-typedef struct players {
-  short player_one;
-  short player_two;
-} players_t;
+extern game_state_t *players[2];
+
+void init_players();
+void add_player(int player_id);
+void destroy_players();
 
 #endif
